f6cef9b5f01e57bb95b224a91075d3eede268763
Clemence Marotte <clemence.marotte@gmail.com>
2025-07-13 23:05:37 +0200

    add banniere + navbar

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+//= link_tree ../images
+//= link application.js
+//= link application.css
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
