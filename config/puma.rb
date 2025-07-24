cee46b53db6897db1c86e0eaf86f20eb65567b15
Clemence Marotte <clemence.marotte@gmail.com>
2025-07-24 18:00:02 +0200

    Fix puma config for Heroku

--- config/puma.rb
+++ config/puma.rb
@@ -4,18 +4,9 @@ threads threads_count, threads_count
 
 preload_app!
 
-# Support IPv6 by binding to host `::` instead of `0.0.0.0`
-port(ENV['PORT'] || 3000, "::")
-
-# Turn off keepalive support for better long tails response time with Router 2.0
-# Remove this line when https://github.com/puma/puma/issues/3487 is closed, and the fix is released
-enable_keep_alives(false) if respond_to?(:enable_keep_alives)
-
-rackup      DefaultRackup if defined?(DefaultRackup)
-environment ENV['RACK_ENV'] || 'development'
+port ENV.fetch('PORT') { 3000 }
+environment ENV.fetch('RAILS_ENV') { 'development' }
 
 on_worker_boot do
-  # Worker-specific setup for Rails 4.1 to 5.2, after 5.2 it's not needed
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 end
