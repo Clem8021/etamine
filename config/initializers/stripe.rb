1409157d08b2bb16ef510fc75fdefbb81743f748
Clemence Marotte <clemence.marotte@gmail.com>
2025-11-03 21:20:55 +0100

    responsive boutique

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,8 @@
 Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
+
+# config/initializers/stripe.rb
+if ENV["FORCE_TEST_CHECKOUT"] == "1"
+  Stripe.api_key = ENV.fetch("STRIPE_SECRET_KEY_TEST")
+else
+  Stripe.api_key = ENV.fetch("STRIPE_SECRET_KEY")
+end
