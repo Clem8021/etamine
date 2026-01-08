f2d52145ce1010c8ccfa9b8d1b30e585f4afc5c1
Clemence Marotte <clemence.marotte@gmail.com>
2026-01-08 14:49:46 +0100

    noël enlevé

--- app/controllers/backoffice/dashboard_controller.rb
+++ app/controllers/backoffice/dashboard_controller.rb
@@ -3,5 +3,15 @@ class Backoffice::DashboardController < ApplicationController
   before_action :require_admin!
 
   def index
+    # Les 5 dernières commandes payées
+    @recent_paid_orders = Order
+      .where(status: "payée")
+      .order(created_at: :desc)
+
+    # Nombre total de commandes payées
+    @paid_orders_count = Order.where(status: "payée").count
+
+    # Nombre total de commandes (tous statuts)
+    @orders_count = Order.count
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -95,28 +95,27 @@ puts "✅ Bouquets de roses créés avec succès (5 à 13 roses)"
 
 puts "Création de la composition florale piquée..."
 
-composition_prices_noel = (25..70).step(5).map { |p| ["#{p}€", p * 100] }.to_h
-composition_prices_jacinthes = (15..70).step(5).map { |p| ["#{p}€", p * 100] }.to_h
+# NOËL //////
+# composition_prices_noel = (25..70).step(5).map { |p| ["#{p}€", p * 100] }.to_h
+# composition_prices_jacinthes = (15..70).step(5).map { |p| ["#{p}€", p * 100] }.to_h
 
-Product.find_or_initialize_by(name: "Composition Florale Piquée Noël").update!(
-  category: "noel",
-  price_cents: 2500, # prix de base si non custom, pas utilisé ici
-  customizable_price: true,
-  price_options: composition_prices_noel,
-  color_options: "rouge et blanc, rose et blanc, vert et blanc, orange saumoné et blanc",
-  image_url: "composition_florale_piquee_noel.jpg"
-)
+# Product.find_or_initialize_by(name: "Composition Florale Piquée Noël").update!(
+  # category: "noel",
+  # price_cents: 2500, # prix de base si non custom, pas utilisé ici
+  # customizable_price: true,
+  # price_options: composition_prices_noel,
+  # color_options: "rouge et blanc, rose et blanc, vert et blanc, orange saumoné et blanc",
+  # image_url: "composition_florale_piquee_noel.jpg"
+# )
 
-Product.find_or_initialize_by(name: "Composition Jacinthes Noël").update!(
-  category: "noel",
-  price_cents: 1500,
-  customizable_price: true,
-  price_options: composition_prices_jacinthes,
-  color_options: "rouge et blanc, rose et blanc, vert et blanc, orange saumoné et blanc",
-  image_url: "composition_jacinthe_noel.jpg"
-)
-
-puts "➡️ Composition florale piquée créée avec succès."
+# Product.find_or_initialize_by(name: "Composition Jacinthes Noël").update!(
+  # category: "noel",
+  # price_cents: 1500,
+  # customizable_price: true,
+  # price_options: composition_prices_jacinthes,
+  # color_options: "rouge et blanc, rose et blanc, vert et blanc, orange saumoné et blanc",
+  # image_url: "composition_jacinthe_noel.jpg"
+#)
 
 composition_prices = (30..120).step(10).map { |p| ["#{p}€", p * 100] }.to_h
 
