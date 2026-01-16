bff9c1572fa74558a98e5d8fbd2b8df136678bd8
Clemence Marotte <clemence.marotte@gmail.com>
2026-01-16 18:21:36 +0100

    solid queue

--- db/migrate/20260116171621_create_solid_queue_jobs.rb
+++ db/migrate/20260116171621_create_solid_queue_jobs.rb
@@ -1,18 +1,19 @@
 class CreateSolidQueueJobs < ActiveRecord::Migration[8.0]
   def change
     create_table :solid_queue_jobs do |t|
-      t.string :queue_name, null: false
-      t.text :arguments
-      t.string :job_class, null: false
-      t.integer :priority, default: 0
+      t.string   :queue_name, null: false
+      t.string   :job_class,  null: false
+      t.text     :arguments
+      t.integer  :priority,   default: 0
+      t.string   :active_job_id        # <- obligatoire pour ActiveJob
       t.datetime :run_at
-      t.datetime :created_at, null: false
-      t.datetime :updated_at, null: false
       t.datetime :failed_at
-      t.text :error
+      t.text     :error
+      t.timestamps
     end
 
     add_index :solid_queue_jobs, :queue_name
     add_index :solid_queue_jobs, :run_at
+    add_index :solid_queue_jobs, :active_job_id
   end
 end
