c9757c640de2037a4c116fdea7e5aaa1b9074d54
Clemence Marotte <clemence.marotte@gmail.com>
2026-01-16 17:59:56 +0100

    stripe

--- app/jobs/stripe_checkout_completed_job.rb
+++ app/jobs/stripe_checkout_completed_job.rb
@@ -3,9 +3,14 @@ class StripeCheckoutCompletedJob < ApplicationJob
 
   def perform(event_json)
     event = JSON.parse(event_json)
+    Rails.logger.info "Stripe event reçu : #{event.inspect}"
+
+    return unless event["data"] && event["data"]["object"]
     session = event["data"]["object"]
+
     order_id = session.dig("metadata", "order_id")
     return unless order_id
+    # order_id ||= 5430  # pour test local si nécessaire
 
     order = Order.find_by(id: order_id)
     return unless order
@@ -20,7 +25,7 @@ class StripeCheckoutCompletedJob < ApplicationJob
       full_name: order.full_name.presence || [delivery&.recipient_firstname, delivery&.recipient_name].compact.join(" ").presence || "Client"
     )
 
-    # emails async
+    # emails async (ou .deliver_now pour test local)
     OrderMailer.confirmation_email(order).deliver_later
     OrderMailer.shop_notification(order).deliver_later
 
