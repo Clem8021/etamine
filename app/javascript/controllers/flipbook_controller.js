d521c6d9b008e18ca6a65744687657f41b0ff5db
Clemence Marotte <clemence.marotte@gmail.com>
2026-03-12 21:16:26 +0100

    page mariage

--- app/javascript/controllers/flipbook_controller.js
+++ app/javascript/controllers/flipbook_controller.js
@@ -1,41 +1,61 @@
-// app/javascript/controllers/flipbook_controller.js
 import { Controller } from "@hotwired/stimulus"
 import { PageFlip } from "page-flip"
 
 export default class extends Controller {
+
+  static targets = ["modal"]
   static values = { width: Number, height: Number, images: Array }
 
   connect() {
-    console.log("=== Flipbook connect ===")
-    console.log("PageFlip défini ?", PageFlip)
+    this.pageFlip = null
+  }
 
-    const container = this.element.querySelector(".flipbook-container")
-    if (!container) {
-      console.error("Pas de container flipbook trouvé")
-      return
+  open() {
+    this.modalTarget.classList.add("active")
+
+    if (!this.pageFlip) {
+      const container = this.modalTarget.querySelector(".flipbook-container")
+
+      this.pageFlip = new PageFlip(container, {
+        width: this.widthValue,
+        height: this.heightValue,
+        size: "stretch",
+        autoSize: true,
+        usePortrait: true,
+        showCover: false,
+        mobileScrollSupport: true,
+        maxShadowOpacity: 0.3
+      })
+
+      this.pageFlip.loadFromImages(this.imagesValue)
     }
 
-    // 📱 Détection mobile
-    const isMobile = window.innerWidth < 768
-
-    // tailles dynamiques
-    const width = isMobile ? 340 : 520
-    const height = isMobile ? 460 : 680
-
-    console.log("Mobile ?", isMobile)
-    console.log("Width :", width, "Height :", height)
-
-    const pageFlip = new PageFlip(container, {
-      width: width,
-      height: height,
-      showCover: false,
-      usePortrait: true,
-      autoSize: true,
-      mobileScrollSupport: true,
-      swipeDistance: 30,
-      maxShadowOpacity: 0.3
-    })
-
-    pageFlip.loadFromImages(this.imagesValue || [])
+    document.addEventListener("keydown", this.handleEsc)
   }
+
+  close() {
+    this.modalTarget.classList.remove("active")
+    document.removeEventListener("keydown", this.handleEsc)
+  }
+
+  outside(event) {
+    if (event.target === this.modalTarget) {
+      this.close()
+    }
+  }
+
+  prev() {
+    this.pageFlip.flipPrev()
+  }
+
+  next() {
+    this.pageFlip.flipNext()
+  }
+
+  handleEsc = (event) => {
+    if (event.key === "Escape") {
+      this.close()
+    }
+  }
+
 }
